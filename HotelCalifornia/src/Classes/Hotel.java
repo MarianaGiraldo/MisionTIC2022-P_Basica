452524d295de84f00eea2757cf445e6e9dbbc198
Mariana <mgiraldo594@misena.edu.co>
2021-07-08 12:22:44 -0500

    Hotel California

---
+++ HotelCalifornia/src/Classes/Hotel.java
@@ -0,0 +1,42 @@
+
+package Classes;
+
+
+public class Hotel {
+   
+    private final Holder holder;
+
+    public Hotel() {
+        this.holder = new Holder();
+    }
+    
+    
+    
+    public void getAvailableRooms(){
+        
+    }
+    
+    
+    public void bookRoom(){
+        
+    }
+    
+        
+    public void deallocateRoom(){
+        
+    }
+    
+    public void featuresRoom(){
+        
+    }
+    
+    public void roomService(){
+        
+    }
+
+    public Holder getHolder() {
+        return holder;
+    }
+    
+    
+}
