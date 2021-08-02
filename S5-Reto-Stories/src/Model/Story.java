e83511e234da85ed4abcc1ee1abf21cff7cc1cf9
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-08-02 11:26:25 -0500

    Reto 5 iniciado

---
+++ S5-Reto-Stories/src/Model/Story.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author maria
+ */
+public class Story {
+    private String id;
+    private String user_id;
+    private String title;
+    private String short_description;
+    private String story;
+    private String timestamp;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShort_description() {
+        return short_description;
+    }
+
+    public void setShort_description(String short_description) {
+        this.short_description = short_description;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public void setStory(String story) {
+        this.story = story;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+}
---
+++ S5-Reto-Stories/src/utils/db/DB.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author maria
+ */
+public class DB {
+    private String user = "root";
+    private String password = "";
+    private String url="jdbc:mysql://localhost:3306/covid";
+    private String driver = "com.mysql.cj.jdbc.Driver";
+
+    public DB() {
+        try {
+            Class.forName( driver );
+            
+            
+        } catch (ClassNotFoundException e) {
+            System.err.println(e);
+            System.err.println("No se puede conectar a la base de datos");
+        }
+    }
+    
+    
+    public Connection createConnection(){
+        Connection conn = null;
+        
+        try {
+            conn = (Connection) DriverManager.getConnection(url, user, password);
+
+        } catch (SQLException e) {
+            System.err.println("Error al obtener el conector: "+e.getMessage());
+        }
+        
+        
+        return conn;
+    }
+    
+    
+}
